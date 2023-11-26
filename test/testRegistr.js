499fca1e1e9c3a81035668d06965a34542f7aba2
dromzi <dlyafailov007@gmail.com>
2023-11-26 14:18:53 +0300

    z

--- test/testRegistr.js
+++ test/testRegistr.js
@@ -1,26 +1,31 @@
-const firstname = 'Артём'
-const lastname = 'Кузьмин'
-const login = 'dromzi89'
-const password = 'dromzi89'
-const passwordConfirm = 'dromzi89'
+function regTest(firstname,lastname,login,password,passwordConfirm) {
 
-if (firstname.length <= 1 || lastname.length <= 1 || login.length <= 5) {
-    showError('Имя,Фамилия,Логин должны быть более 5 символов');
-    return;
-}
+    if (login.length <= 5) {
+        console.log("\x1b[31m", 'false');
+        console.log('Логин должны быть более 5 символов');
+        return;
+    }
+    
+    if (firstname.length <= 1 || lastname.length <= 1) {
+        console.log("\x1b[31m", 'false');
+        console.log("Имя, Фамилия должны быть больше одного символа");
+        return;
+    }
+    
+    if (password.length < 6) {
+        console.log("\x1b[31m", 'false');
+        console.log('Пароль должен быть не менее 6 символов');
+        return;
+    }
+    
+    if (password !== passwordConfirm) {
+        console.log("\x1b[31m", 'false');
+        console.log('Пароли не совпадают');
+        return; 
+    }
   
-if (password.length < 6) {
-    showError('Пароль должен быть не менее 6 символов');
-    return;
-}
+    console.log("\x1b[32m", 'true');
+    console.log("Пользователь зарегистрирован");
+  }
   
-if (password !== passwordConfirm) {
-    showError('Пароли не совпадают');
-    return; 
-}
-
-return console.log("Пользователь зарегистрирован");
-
-function showError(text) {
-    console.log(text);
-}
+  regTest("Артём", "Кузьмин", "dromzi89", "makaka123-", "makaka123-")
